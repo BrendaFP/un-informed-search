71b1b9eb0db8ed6972da2ced368ae487c7291bfe
Brenda Flores <a01206527@itesm.mx>
2019-03-01 05:57:56 +0000

    Add base case

--- ucs.py
+++ ucs.py
@@ -8,9 +8,20 @@ if __name__ == "__main__":
     initial = list(map(convert_array,input().split(';')))
     goal = list(map(convert_array,input().split(';')))
   
-    print(max_height)
-
-    for x in range(len(initial)): 
-        print initial[x],
-    for x in range(len(goal)): 
-        print goal[x], 
+    #print(max_height)
+    #print(len(initial))
+    #print(len(goal))
+    
+    for x in range(len(initial)):
+        if(len(initial[x])>=max_height):
+            print("No solution found")
+            exit()
+    for x in range(len(goal)):
+        if(len(goal)>=max_height):
+            print("No solution found")
+            exit()
+    
+    #for x in range(len(initial)): 
+    #    print (initial[x][0])
+    #for x in range(len(goal)): 
+    #    print (goal[x])
